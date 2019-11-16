e12735a79cd80eb1aa0dd2dbe0ec1756db0b1d82
Matthew Jones <matts3371@gmail.com>
2019-11-16 23:16:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday|
     holiday << "Balloons"
+    binding.pry 
   end
 end
 
